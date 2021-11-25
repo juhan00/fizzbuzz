92cf0a53d25025b2ab00c88c4afef3b9b1227d80
juhan00 <jvabya@gmail.com>
2021-11-25 13:53:20 +0900

    feat: Print numbers with for statement(main)
    range: 1 to 15
    type: string

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,7 @@
 def do_fizzbuzz():
-    print('Do fizzbuzz!')
-    return True
+    for i in range(1,15+1);
+        print('{}'.format(i))
+    return True   
 
 if __name__=='__main__':
     do_fizzbuzz()
